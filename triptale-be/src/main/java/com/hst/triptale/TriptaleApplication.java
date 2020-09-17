23e7a18584e3f75c04d4a414a61b91437d85039d
HyunGyu-Lee <gusrb0808@naver.com>
2020-09-17 23:50:20 +0900

    [BE] Spring Boot Initialize

---
+++ triptale-be/src/main/java/com/hst/triptale/TriptaleApplication.java
@@ -0,0 +1,13 @@
+package com.hst.triptale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptaleApplication.class, args);
+	}
+
+}
---
+++ triptale-be/src/test/java/com/hst/triptale/TriptaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hst.triptale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
