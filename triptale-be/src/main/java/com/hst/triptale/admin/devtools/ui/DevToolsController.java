fc074564a02ef8e9c8d787033d7d1a6aa7d3c4f4
shl9012 <zoflrjs2145@naver.com>
2020-11-03 01:08:50 +0900

    [#14] 개발자 편의기능 - 토큰발급

---
+++ triptale-be/src/main/java/com/hst/triptale/admin/devtools/ui/DevToolsController.java
@@ -0,0 +1,38 @@
+package com.hst.triptale.admin.devtools.ui;
+
+import com.hst.triptale.configuration.ApplicationConstants;
+import com.hst.triptale.content.user.service.UserService;
+import com.hst.triptale.security.EnableProfiles;
+import com.hst.triptale.security.token.JwtAuthenticationTokenProvider;
+import com.hst.triptale.security.token.model.UserAuthenticationToken;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author lyoupyo@gmail.com
+ */
+@Tag(name = "개발자 편의 기능 API", description = "개발자 편의용 기능 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(ApplicationConstants.APIGroups.ADMIN_API + "/devtools")
+public class DevToolsController {
+
+    private final JwtAuthenticationTokenProvider jwtAuthenticationTokenProvider;
+
+    @Operation(
+            summary = "사용자 정보 조회",
+            parameters = {
+                    @Parameter(name = "no", description = "사용자 번호", in = ParameterIn.PATH)
+            }
+    )
+    @GetMapping("/issueToken/{no}")
+    @EnableProfiles(profile = {"local", "develop"})
+    public UserAuthenticationToken issueUserToken(@PathVariable Long no) {
+        return jwtAuthenticationTokenProvider.issue(no);
+    }
+
+}
