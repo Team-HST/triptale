644f3d7851dfa6e312c76907d5d7e224f4d36677
HyunGyu-Lee <gusrb0808@naver.com>
2021-02-17 23:34:17 +0900

    여행 일차 API에 장소 목록 추가

--- triptale-be/src/main/java/com/hst/triptale/content/trip/ui/response/DayScheduleResponse.java
+++ triptale-be/src/main/java/com/hst/triptale/content/trip/ui/response/DayScheduleResponse.java
@@ -2,6 +2,7 @@ package com.hst.triptale.content.trip.ui.response;
 
 import java.time.LocalDate;
 
+import com.hst.triptale.content.place.ui.response.PlaceListResponse;
 import com.hst.triptale.content.schedule.entity.DaySchedule;
 
 import lombok.Builder;
@@ -18,6 +19,7 @@ public class DayScheduleResponse {
 	private final String description;
 	private final String colorCode;
 	private final LocalDate date;
+	private final PlaceListResponse places;
 
 	public static DayScheduleResponse from(DaySchedule daySchedule) {
 		return DayScheduleResponse.builder()
@@ -26,6 +28,7 @@ public class DayScheduleResponse {
 			.description(daySchedule.getDescription())
 			.colorCode(daySchedule.getColorCode())
 			.date(daySchedule.getScheduleDate())
+			.places(PlaceListResponse.of(daySchedule, daySchedule.getPlaces()))
 			.build();
 	}
 }
